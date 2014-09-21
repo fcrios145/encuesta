886e06cb58dfb9b6a2dd9d52c2e03fe428a8e230
Francisco Rios <fcrios145@hotmail.com>
2014-09-21 01:02:36 -0600

    tecnica secret key

--- encuestas/encuestas/settings.py
+++ encuestas/encuestas/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x$rvv-)wj^b_a#fpbs!a-0megxsza&t^-pm4+^9nao)%tgu-%e'
+SECRET_KEY = os.environ["SOME_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'encuestas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'encuestas',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
