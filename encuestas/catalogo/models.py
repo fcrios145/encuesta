0564d27d0dfe3ada6435d39e31e7c46258816de0
Francisco Rios <fcrios145@hotmail.com>
2014-09-21 01:18:31 -0600

    clase abstracta TimeStamp

--- encuestas/catalogo/models.py
+++ encuestas/catalogo/models.py
@@ -1,3 +1,13 @@
+#core/models.py
 from django.db import models
 
-# Create your models here.
+
+class TimeStampModel(models.Model):
+    """
+    clase base abstracta que actualiza de manera automatica los campos created y modified
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
