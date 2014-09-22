a828fb04e5253e52fa296ebe8dddd6c0c4eda532
Francisco Rios <fcrios145@hotmail.com>
2014-09-21 21:56:43 -0600

    agregada todas los modelos al admin

--- encuestas/catalogo/admin.py
+++ encuestas/catalogo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Catalogo, Persona, Pregunta, Respuesta
+from .models import Catalogo, Persona, Pregunta, Respuesta, Seleccion, Carrera
 
 # Register your models here.
 
@@ -7,3 +7,5 @@ admin.site.register(Catalogo)
 admin.site.register(Persona)
 admin.site.register(Pregunta)
 admin.site.register(Respuesta)
+admin.site.register(Seleccion)
+admin.site.register(Carrera)
